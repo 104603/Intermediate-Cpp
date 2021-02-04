1ee66af04049ecdf420b41c08a1e2a3b55281925
Bablu Kavali <abhishekreddy909@gmail.com>
2021-02-04 23:18:15 +0530

    Initial

---
+++ calculator/calculator.h
@@ -0,0 +1,9 @@
+#ifndef __CALC_H__
+#define __CALC_H__
+
+int addition(int operand1, int operand2);
+int subtraction(int operand1, int operand2);
+int multiplication(int operand1, int operand2);
+int division(int operand1, int operand2);
+
+#endif
